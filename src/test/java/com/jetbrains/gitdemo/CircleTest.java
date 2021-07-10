e78f0564d08cea20b37885691276a37880561ad5
kwikwa <malaouif@hotmail.com>
2021-07-10 18:51:54 -0400

    Update CircleTest.java

--- src/test/java/com/jetbrains/gitdemo/CircleTest.java
+++ src/test/java/com/jetbrains/gitdemo/CircleTest.java
@@ -11,7 +11,7 @@ class CircleTest {
         var circle = new Circle();
 
         // expect
-        Assertions.assertEquals(100, circle.getNumberOfVertices());
+        Assertions.assertEquals(20000, circle.getNumberOfVertices());
     }
 
     @Disabled("Not implemented")
